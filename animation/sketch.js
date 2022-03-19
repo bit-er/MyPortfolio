f6b3fec387728c0d75d81d9c8d226d7d135dafb2
Esmail <mti123@student.bau.edu.lb>
2022-03-19 21:57:39 +0200

    fixed canvas

--- animation/sketch.js
+++ animation/sketch.js
@@ -1,20 +1,21 @@
-var x=[],y=[],a=[],s=[],d=[];
+var x=[],y=[],a=[],s=[],d=[],numberOfDigit=1;
 
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
+
 }
 function setup() {
-  // put setup code here
 
-  const canvas =createCanvas(windowWidth,windowHeight*5.3);
+
+  const canvas =createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index',-1);
   
-  for(let i=0;i<10;i++){
+  for(let i=0;i<numberOfDigit;i++){
     x.push(random(1));
-    y.push(random(-1000 ,-10));
-    a.push(random(1,10));
-    s.push(random(20,30));
+    y.push(random(-100 ,-10));
+    a.push(random(1,5));
+    s.push(random(15,20));
     d.push(int(round(random(1))));
   }
   
@@ -24,14 +25,23 @@ function setup() {
 
 function draw() {
 
-  // put drawing code here
+
   background(255,255);
-  
-  
-  for(let i=0;i<10;i++){
+  for(let i=0;i<numberOfDigit;i++){
+
     textSize(s[i]);
     text(d[i],x[i]*windowWidth,y[i]);
-    y[i]=(y[i]>windowHeight*5.3)?-50:y[i]+a[i];
+
+    if(y[i]>windowHeight){
+          x[i]=random(1);
+          y[i]=random(-100 ,-10);
+          a[i]=random(1,5);
+          s[i]=random(15,20);
+          d[i]=int(round(random(1)));
+    }
+    else{
+          y[i]+=a[i];
+    }
   }
 
 }
